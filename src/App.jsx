4f3b092ce38d357bad664faac1bbb0bfb35cbda7
dmystical-coder <dmystical.coder@gmail.com>
2025-01-29 22:09:57 +0100

    feat: Add wallet connection and balance management with notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
+import toast, { Toaster } from 'react-hot-toast';
 import { useState, useEffect } from 'react'
 import { ethers } from 'ethers'
 import abi from './abi.json'
+
 
 
 const App = () => {
   const contractAddress = '0x8bdC49446391D8D6ee54C82fE625553017A89805'
   const [balance, setBalance] = useState(0);
   const [amount, setAmount] = useState("");
-  const [provider, setProvider] = useState(null);
-  const [signer, setSigner] = useState(null);
-  const [myContract, setMyContract] = useState(null);
   const [address, setAddress] = useState(null);
 
   const requestAccounts = async () => {
@@ -25,7 +24,9 @@ const App = () => {
         const provider = new ethers.BrowserProvider(window.ethereum);
         const accounts = await provider.send("eth_requestAccounts", []);
         setAddress(accounts[0]); // Set the first account connected
+        toast.success("Wallet Connected Successfully!");
       } catch (error) {
+        toast.error("Error connecting Wallet")
         console.error("Error connecting wallet:", error);
       }
     } else {
@@ -33,75 +34,86 @@ const App = () => {
     }
   }
 
+  // const provider = new ethers.BrowserProvider(window.ethereum)
+  // const myContract = new ethers.Contract(contractAddress, abi, provider)
+  // myContract.on("Deposit", (amount, event) => {
+  //   toast.success(`${ethers.formatEther(amount)} ETH successfully deposited!`);
+  //   event.removeListener;
+  // });
 
-  useEffect(() => {
-    const initProvider = async () => {
-      if (typeof window.ethereum !== 'undefined') {
-
-        await requestAccounts()
-
-        const _provider = new ethers.BrowserProvider(window.ethereum)
-        const _signer = await provider.getSigner()
-        const _myContract = new ethers.Contract(contractAddress, abi, signer)
-
-        setProvider(_provider);
-        setSigner(_signer);
-        setMyContract(_myContract);
-      }
-    };
-    initProvider();
-  }, []);
 
 
   const fetchBalance = async () => {
-    if (myContract) {
-      const _balance = await myContract.getBalance();
-      setBalance(ethers.utils.formatEther(_balance));
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccounts()
+
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      const myContract = new ethers.Contract(contractAddress, abi, provider)
+      const balance = await myContract.getBalance();
+      setBalance(ethers.formatEther(balance));
+      toast.success("Balance updated!")
     }
   };
 
   const handleDeposit = async () => {
-    if (myContract) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccounts()
+
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      const signer = await provider.getSigner()
+      const myContract = new ethers.Contract(contractAddress, abi, signer)
       try {
-        const tx = await myContract.deposit({ value: ethers.utils.parseEther(amount) });
+        const tx = await myContract.deposit(ethers.parseEther(amount), {
+          value: ethers.parseEther(amount)
+        });
         await tx.wait();
+        toast.success(`${ethers.formatEther(amount)} ETH successfully deposited!`);
         fetchBalance();
-        alert("Deposit successful!");
       } catch (error) {
         console.error("Error during deposit:", error);
-        alert("Deposit failed. See console for details.");
+        toast.error("Error during deposit");
       }
+      setAmount("")
     }
   };
 
   const handleWithdraw = async () => {
-    if (myContract) {
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccounts()
+
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      const signer = await provider.getSigner()
+      const myContract = new ethers.Contract(contractAddress, abi, signer)
       try {
-        const tx = await myContract.withdraw(ethers.utils.parseEther(amount));
+        const tx = await myContract.withdraw(ethers.parseEther(amount));
         await tx.wait();
+        toast(`${ethers.formatEther(amount)} ETH successfully withdrawn!`);
         fetchBalance();
-        alert("Withdrawal successful!");
       } catch (error) {
         if (error.reason === "execution reverted: InsufficientBalance") {
-          alert("Insufficient balance.");
+          toast.error("Insufficient balance.");
         } else {
           console.error("Error during withdrawal:", error);
-          alert("Withdrawal failed. See console for details.");
+          toast.error("Withdrawal failed. See console for details.");
         }
+        setAmount("")
       }
     }
   };
 
   useEffect(() => {
-    if (myContract) fetchBalance();
-  }, [myContract]);
+    if (address) {
+      fetchBalance();
+    }
+  }, [address]);
 
 
   return (
     <>
+      <Toaster position="top-center"
+        reverseOrder={false} />
       <div className="app">
-        <h1>DApp Interface</h1>
-
+        <h1>Wallet DApp</h1>
         {!address ? (
           <button className="connect-button" onClick={connectWallet}>
             Connect Wallet
@@ -110,21 +122,21 @@ const App = () => {
           <p className="wallet-address">Connected Wallet: {address}</p>
         )}
 
-        <p>Connected Address Balance: {balance} ETH</p>
+        <p>Contract Balance: {balance} ETH</p>
 
 
         <div>
           <input
-            type="text"
+            type="number"
             placeholder="Enter amount in ETH"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
           />
         </div>
 
-        <div>
-          <button onClick={handleDeposit}>Deposit</button>
-          <button onClick={handleWithdraw}>Withdraw</button>
+        <div className='btn-group'>
+          <button role="button" onClick={handleDeposit}>Deposit</button>
+          <button role="button" onClick={handleWithdraw}>Withdraw</button>
         </div>
       </div>
     </>
